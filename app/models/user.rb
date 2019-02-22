5cf1abaa327ff945fad61f12100266cb8e33a6e5
Per Eriksson <per_hegg@hotmail.com>
2019-02-22 16:39:27 +0100

    installs and sets up carfify

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 8 }
   validates_presence_of :email, :password
   validates_uniqueness_of :email
+
+  class User < ApplicationRecord
+    has_many :orders, class_name: 'Cartify::Order', foreign_key: :user_id
+    has_one :billing, class_name: 'Cartify::Billing', foreign_key: :user_id
+    has_one :shipping, class_name: 'Cartify::Shipping', foreign_key: :user_id
+    has_many :addresses, class_name: 'Cartify::Address', foreign_key: :user_id
+  end
 end
